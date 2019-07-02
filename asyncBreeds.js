47aafc922a8920ec724e828b0f104007fbaeb75c
Tyler Caceres <tyler.caceres@gmail.com>
2019-07-02 22:18:08 +0000

    async and sync test and files functioning correctly

---
+++ asyncBreeds.js
@@ -0,0 +1,22 @@
+// asyncBreeds.js
+const fs = require("fs");
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log("breedDetailsFromFile: Calling readFile...");
+  fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
+    // CHANGE 1: Pass data back via callback instead of return.
+    console.log("Callback: I have the data!");
+    if (error) {
+      callback(undefined);
+    }
+    if (!error) {
+      callback(data);
+    }
+  });
+};
+
+module.exports = breedDetailsFromFile;
+
+// breedDetailsFromFile("Bombay", data => {
+//   console.log("the data here:", data);
+// });
