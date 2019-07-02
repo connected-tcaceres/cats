e503fd3665c891f273e1bbf8e921ccc1aa30c6c7
Tyler Caceres <tyler.caceres@gmail.com>
2019-07-02 22:39:08 +0000

    understand completed

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -20,3 +20,9 @@ module.exports = breedDetailsFromFile;
 // breedDetailsFromFile("Bombay", data => {
 //   console.log("the data here:", data);
 // });
+
+// breedDetailsFromFile("Bombay", bombay => {
+//   const expectedDesc =
+// 		"The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+//   assert.equal(expectedDesc, bombay);
+// });
